fe9ece7a537c445c25f64711f71dd3e03292383c
mab538 <mike.bosland@gmail.com>
2021-04-24 00:46:59 -0700

    Removed esxi references and unnecessary code

--- provider.tf
+++ provider.tf
@@ -4,24 +4,8 @@ terraform {
           source = "digitalocean/digitalocean"
           version = "1.22.2"
       }
-
-      esxi = {
-        source = "josenk/esxi"
-      }
     }
 }
-
-# ESXi (Local)
-
-provider "esxi" {
-  alias = "esxi4"
-  esxi_hostname = var.esxi4_hostname
-  esxi_hostport = var.esxi4_hostport
-  esxi_hostssl = var.esxi4_hostssl
-  esxi_username = var.esxi4_username
-  esxi_password = var.esxi4_password
-}
-
 
 # DIGITALOCEAN (Remote)
 provider "digitalocean" {
